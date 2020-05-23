aaefe724e48876bbb7b8b9f6a6332756939252a8
Janis Feldmanis <feldmanis45@gmail.com>
2020-05-23 16:48:52 +0300

    added login page

---
+++ ui/Login/login.js
@@ -0,0 +1,11 @@
+
+username = document.getElementsByName("username");
+password = document.getElementsByName("password");
+
+function getUserCrendintal(){
+    username = document.getElementById("username");
+    password = document.getElementById("password");
+    console.log("Username: ",username.value);
+    console.log("Password: ",password.value);
+    console.log("test");
+}
